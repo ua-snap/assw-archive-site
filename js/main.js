05e46bb6709fe7fb69dcce777f0f95855406a8d5
Bruce Crevensten <bruce.crevensten@gmail.com>
2016-03-24 10:08:40 -0800

    adds image gallery & news toggle

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+$(document).ready(function () {
+	$("#nanoGallery1").nanoGallery({
+		kind: 'flickr',
+		userID: '133161750@N07',
+		photoset: 'none',
+
+		thumbnailWidth: 'auto',
+		thumbnailHeight: 100,
+
+		thumbnailHoverEffect: [{ name: 'labelAppear75', duration: 300 }],
+		theme: 'light',
+		thumbnailGutterWidth : 0,
+		thumbnailGutterHeight : 0,
+		i18n: { thumbnailImageDescription: 'View Photo', thumbnailAlbumDescription: 'Open Album' },
+		thumbnailLabel: { display: true, position: 'overImageOnMiddle', align: 'center' }
+	});
+
+	$("#toggleNews").on('click', function() {
+		$(".extra").toggle();
+		$("#toggleNews").hide();
+	});
+
+});
 
